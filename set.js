c3d5bed4df534ffae94cf20cc39bae815b7e6a27
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-06-26 16:00:05 +0200

    set

--- set.js
+++ set.js
@@ -1,4 +1,4 @@
-function mySet(){
+function Set(){
 
     var collection = [];
 
@@ -26,4 +26,69 @@ function mySet(){
         }
         return false;
     }
+
+    // this method will return the size of the array
+    this.size = function(){
+        return collection.length;
+    }
+
+    // this method will return a union of two sets
+    this.union = function(otherSet){
+        var unionSet = new Set();
+        var firstSet = this.values();
+        var secondSet = otherSet.values();
+
+        firstSet.forEach(element => {
+            unionSet.add(element);
+        });
+
+        secondSet.forEach(element => {
+            unionSet.add(element);
+        })
+
+        return unionSet;
+    }
+
+    //this method will return the intersection of two sets as a new set
+    this.intersection = function(){
+        var intersectionSet = new Set();  
+        var firstSet = this.values();
+        firstSet.forEach(element => {
+            if(otherSet.has(element)){
+                intersectionSet.add(element);
+            }
+        });
+        return intersectionSet;     
+    }
+
+    this.difference = function(otherSet){
+        var intersectionSet = new Set();  
+        var firstSet = this.values();
+        firstSet.forEach(element => {
+            if(!otherSet.has(element)){
+                intersectionSet.add(element);
+            }
+        });
+        return intersectionSet; 
+    }
+
+    // this method will test if the set is a subsiet of  a different set
+    this.subset = function(otherSet){
+        var firstSet = this.values();
+
+        return firstSet.every(function(value){
+            return otherSet.has(value);
+        });
+    }
 }
+
+var setA = new Set();
+var setB = new Set();
+
+setA.add('a');
+setB.add('b');
+setB.add('c');
+setB.add('a');
+setB.add('d');
+
+console.log(setA.subset(setB));
