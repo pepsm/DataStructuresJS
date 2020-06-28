7d06ac074e92f4747c758fb11fc92dccb336afe8
Mihaylov,Petar P <p.mihaylov@student.fontys.nl>
2020-06-28 09:58:01 +0200

    PreOrder PostOrder InOreder search

---
+++ heightBinaryTree.js
@@ -0,0 +1,169 @@
+class Node {
+    constructor(data, left = null, right = null){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BST{
+    constructor(){
+        this.root = null;
+    }
+
+    add(data){
+        const node = this.root;
+        if(node === null){
+            this.root = new Node(data);;
+            return;
+        }else {
+
+            // recusive function
+            const searchTree = function(node){
+                if(data < node.data){
+                    if(node.left === null){
+                        node.left = new Node(data);
+                        return;
+                    }else if(node.left !== null){
+                        return searchTree(node.left);
+                    }
+                }else if(data > node.data){
+                    if(node.right === null){
+                        node.right = new Node(data);
+                        return;
+                    }else if(node.right !== null){
+                        return searchTree(node.right);
+                    }
+                }else {
+                    return null;
+                }
+            };
+
+            // invoke the function
+            return searchTree(node); 
+        }
+    }
+
+    remove(data){
+        const removeNode = function(node, data){
+            if(node == null){
+                return null;
+            }
+
+            if(data == node.data){
+                if(node.left == null && node.right == null){
+                    return null; 
+                }
+
+                // node has no left child
+                if(node.left == null){
+                    return node.right;
+                }
+
+                // node has no right child
+                if(node.right == null){
+                    return node.left;
+                }
+
+                // node has two children
+                var tempNode = node.right;
+                while(tempNode.left != null){
+                    tempNode = tempNode.left;
+                }
+
+                node.data = tempNode.data;
+                node.right = removeNode(node.right, tempNode.data);
+                return node;
+            }else if(data < node.data){
+                node.left = removeNode(node.left, data);
+                return node;
+            }else {
+                node.right = removeNode(node.right, data);
+                return node;
+            }
+        }
+
+        this.root = removeNode(this.root, data);
+    }
+
+    isBalanced(){
+        return (this.findMinHeight() >= this.findMaxHeight() - 1);
+    }
+
+    findMinHeight(node = this.root){
+        if(node == null){
+            return -1;
+        }
+        let left = this.findMinHeight(node.left);
+        let right = this.findMinHeight(node.right);
+
+        if(left < right){
+            return left + 1;
+        } else {
+            return right + 1;
+        }
+    }
+
+    findMaxHeight(node = this.root){
+        if(node == null){
+            return -1;
+        }
+
+        let left = this.findMaxHeight(node.left);
+        let right = this.findMaxHeight(node.right);
+
+        if(left > right){
+            return left + 1;
+        }else{
+            return right + 1;
+        }
+    }
+
+    inOrder(){
+        if(this.root == null){
+            return null;
+        }else {
+            var result = new Array();
+            function traverseInOrder(node){
+                node.left && traverseInOrder(node.left);
+                result.push(node.data);
+                node.right && traverseInOrder(node.right);
+            }
+            traverseInOrder(this.root);
+                return result;
+            
+        }
+    }
+
+    preOrder(){
+        if(this.root == null){
+            return null;
+        }else {
+            var result = new Array();
+            function traversePreOrder(node){
+                result.push(node.data);
+                node.left && traversePreOrder(node.left);
+                node.right && traversePreOrder(node.right);
+            }
+            traversePreOrder(this.root);
+                return result;
+            
+        }
+    }
+
+    postOrder(){
+        if(this.root == null){
+            return null;
+        }else {
+            var result = new Array();
+            function traversePostOrder(node){
+                node.left && traversePostOrder(node.left);
+                node.right && traversePostOrder(node.right);
+                result.push(node.data);
+            }
+            traversePostOrder(this.root);
+                return result;
+            
+        }
+    }
+}
